a99fb286a9a87f5491deaed5991c692a893ae113
Jacob Johnson <johjacb@bethel.edu>
2014-10-14 08:42:42 -0500

    adding .gitignore

---
+++ src/js/accordion.js
@@ -0,0 +1,20 @@
+openNav = function(e) {
+
+    e.preventDefault();
+
+    // Toggles <button> ^ <li> class
+    this.parentNode.classList.toggle('accordion--active');
+
+    // Moves up a node from <button> ^ <li> then next to <ul>
+    var subList = this.parentNode.nextElementSibling;
+
+    // Toggles <button> ^ <li> + <ul> class 
+    subList.classList.toggle('accordion-list--open');
+
+    // Changes button text    
+    if (this.parentNode.classList.contains("accordion--active")){
+        this.innerHTML = "-";
+    } else {
+        this.innerHTML = "+";
+    }
+};
