827e2d3c374dabb2cf96d78545de6804dd87017c
Eric Jameson <e-jameson@bethel.edu>
2015-03-12 13:43:44 -0500

    add bu_animatej.s

--- src/js/odometer.js
+++ src/js/odometer.js
@@ -4,7 +4,23 @@
    function markScroll() {
        didScroll = true;
    }
-   
+
+   function findProofPointCollectionParentNode(childObj){
+       var testObj = childObj;
+       var count = 1;
+       while(true) {
+           testObj = testObj.parentNode;
+           if(!testObj.classList) {
+               continue;
+           }
+           if(testObj.classList.contains('proof-point-collection')){
+               break;
+           }
+       }
+       // go up one more because we need to add to the section
+       return testObj.parentNode;
+   }
+
    window.onload = function(){
        if(document.getElementsByClassName('odometer').length > 0){
            intervalID = setInterval(function() {
@@ -18,7 +34,9 @@
            doAnimations();
        }   
    }
-   
+
+
+
    // Function which adds the 'animated' class to any '.animatable' in view
    var doAnimations = function() {
        // Calc current offset and get all animatables
@@ -35,6 +53,14 @@
            var odometer = odometers[index];
            var scrollPos = odometer.getBoundingClientRect().top + document.body.scrollTop - document.body.clientTop + odometer.offsetHeight - 20;
            if (scrollPos < offset) {
+               var ppcParent = findProofPointCollectionParentNode(odometer);
+               // don't add twice
+               if(!ppcParent.classList){
+                   ppcParent.className = "js-animate"
+               }
+               else if(!ppcParent.classList.contains('js-animate')){
+                ppcParent.className = ppcParent.className + " js-animate"
+               }
                odometer.innerHTML = odometer.dataset.finalNumber;
                odometer.className = odometer.className + " odometer_done"
            }
