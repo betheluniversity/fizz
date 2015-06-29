1a80188cec9a36e214197f82af1aaa73ba690308
Jacob Johnson <johjacb@bethel.edu>
2015-06-29 15:42:09 -0500

    temp removing responsive tables js

--- src/js/main.js
+++ src/js/main.js
@@ -21,33 +21,33 @@ window.onload = function() {
      }
 };
 
-// forEach method, could be shipped as part of an Object Literal/Module
-var forEach = function (array, callback, scope) {
-  for (var i = 0; i < array.length; i++) {
-    callback.call(scope, i, array[i]); // passes back stuff we need
-  }
-};
+// // forEach method, could be shipped as part of an Object Literal/Module
+// var forEach = function (array, callback, scope) {
+//   for (var i = 0; i < array.length; i++) {
+//     callback.call(scope, i, array[i]); // passes back stuff we need
+//   }
+// };
 
-var tableList = document.querySelectorAll("table.responsive");
-forEach(tableList, function (index, value) {
-  console.log(index, value); // passes index + value back!
+// var tableList = document.querySelectorAll("table.responsive");
+// forEach(tableList, function (index, value) {
+//   console.log(index, value); // passes index + value back!
 
-  var headertext = [],
-      headers = value.querySelectorAll("th"),
-      tablerows = value.querySelectorAll("th"),
-      tablebody = value.querySelector("tbody");
+//   var headertext = [],
+//       headers = value.querySelectorAll("th"),
+//       tablerows = value.querySelectorAll("th"),
+//       tablebody = value.querySelector("tbody");
 
-  for(var i = 0; i < headers.length; i++) {
-    var current = headers[i];
-    headertext.push(current.textContent.replace(/\r?\n|\r/,""));
-  };
+//   for(var i = 0; i < headers.length; i++) {
+//     var current = headers[i];
+//     headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+//   };
 
-  for (var k = 0, row; row = tablebody.rows[k]; k++) {
-    for (var j = 0, col; col = row.cells[j]; j++) {
-      col.setAttribute("data-th", headertext[j]);
-    } 
-  };
-});
+//   for (var k = 0, row; row = tablebody.rows[k]; k++) {
+//     for (var j = 0, col; col = row.cells[j]; j++) {
+//       col.setAttribute("data-th", headertext[j]);
+//     } 
+//   };
+// });
 
 // if( document.getElementsByClassName('js-rotate-order-carousel')[0] ){
 // 	if(typeof(Storage) !== "undefined") {
