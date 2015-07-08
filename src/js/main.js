a930105b388dd83e20bd16fb48b59a711c301ec1
Eric Jameson <e-jameson@bethel.edu>
2015-07-08 14:59:00 -0500

    foreach so we catch all .odometer

--- src/js/main.js
+++ src/js/main.js
@@ -19,10 +19,13 @@ window.onload = function() {
         var s = skrollr.init();
     }
     // Odometer init
-    var el = document.querySelector('.odometer');
-    od = new Odometer({
-        el: el
-    });
+    var odometers = document.getElementsByClassName('odometer');
+    for (var i = 0, len = odometers.length; i < len; i++) {
+        var el = odometers[i];
+        var od = new Odometer({
+            el: el
+        });
+    }
 };
 
 
