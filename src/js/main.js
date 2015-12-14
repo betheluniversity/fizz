6d8a3520f0264951ab31d2bb23f7ae710b27e043
Jacob Johnson <johjacb@bethel.edu>
2015-12-14 15:27:44 -0600

    carousel tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -76,9 +76,9 @@ for (var i = 0, len = carousels.length; i < len; i++) {
         cellAlign: 'left'
     });
     
-    flkty.on('cellSelect', function(){
-        isSelected();
-    })
+    // flkty.on('cellSelect', function(){
+    //     isSelected();
+    // })
 };
 
 function onLoadeddata( event ) {
@@ -100,9 +100,9 @@ if( typeof flkty !== 'undefined' ) {
 
 // Checking for 'is-selected' on load
 
-addEvent(window, 'load', function() {
-    isSelected();
-});
+// addEvent(window, 'load', function() {
+//     isSelected();
+// });
 
 // This function needs a double test because some carousels
 // may not have the .lazyload class applied on load. If it already
