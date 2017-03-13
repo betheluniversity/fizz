049a719c7aef990739dcd7051a6714f10d4ccf75
Jacob Johnson <johjacb@bethel.edu>
2017-03-13 14:01:30 -0500

    changes

--- src/js/info-tags.js
+++ src/js/info-tags.js
@@ -1,32 +1,79 @@
 var infoTags = document.querySelector('.info-tags');
 
-toggleInfoTag = function() {
-	console.log(i);
-	infoTagsContentPanel[i].classList.toggle('active');
-};
-
 if(infoTags){
 
 	var infoTagsLink = infoTags.querySelectorAll('.info-tags__link');
 	var infoTagsContentPanel = infoTags.querySelectorAll('.info-tags__content-panel');
 
-	document.body.addEventListener('click', function (event) {
-	    if (infoTags.contains(event.target)) {
-	        if(infoTags.classList.contains('active')) {
-	        } else {
-	        	infoTags.classList.add('active')
-	        }
+	document.body.addEventListener('click', function(e) {
+	    if (infoTags.contains(event.target)) { 
+	        // toggleInfoTag(e);
+	        // infoTags.classList.add('active');
+	        // openTags();
+	        openPanel(e);
 	    } else {
-	        console.log('clicked outside');
-	        infoTags.classList.remove('active');
+	        shutDown();
 	    }
 	});
 
-	for (i=0;i<infoTagsLink.length;i++){
-		infoTagsLink[i].addEventListener('click', function (x) {
-	        return function (e) {
-	            toggleInfoTag(x, e);
-	        };
-	    })(i);
+	openTags = function() {
+		infoTags.classList.add('active');
 	}
+
+	openPanel = function(e){
+		var panNum = e.path[0].id;
+		var panActive = document.querySelector(panNum).classList.contains('active');
+
+		if(panActive){
+			// shutDown();
+		} else {
+			openTags();
+			document.querySelector(panNum).classList.add('active');
+		}
+	}
+
+	shutDown = function(i){
+		infoTags.classList.remove('active');
+		// infoTags.addEventListener("transitionend", function(event) {
+			for (var i=0;i<infoTagsContentPanel.length;i++){
+				infoTagsContentPanel[i].classList.remove('active');
+			}
+		// }, false);
+	}
+
+
+
+
+	// toggleInfoTag = function(e) {
+	// 	var panNum = e.path[1].id;
+	// 	var infoTagsActive = document.querySelector('.info-tags.active');
+		
+	// 	for (var i=0;i<infoTagsContentPanel.length;i++){
+
+	// 		var loopNum = i;
+
+	// 		if(infoTagsContentPanel[i].id == panNum){
+
+	// 			if(infoTagsContentPanel[i].classList.contains('active') && infoTagsActive){
+
+	// 				infoTagsActive.classList.remove('active');
+
+	// 				var erlement = infoTagsContentPanel[i];
+	// 				infoTagsActive.addEventListener("transitionend", function(event) {
+	// 					erlement.classList.remove('active');
+
+	// 				}, false);
+
+	// 				// infoTagsContentPanel[i].classList.remove('active');
+	// 			} else {
+	// 				infoTagsContentPanel[i].classList.add('active');
+	// 				infoTags.classList.add('active');
+	// 			}
+	// 		} else {
+	// 			infoTagsContentPanel[i].classList.remove('active');
+	// 		}
+	// 	}
+	// };
+
+
 }
