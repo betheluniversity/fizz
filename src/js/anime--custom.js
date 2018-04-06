760220f621fdba9392262e183ee8f40045cda10a
Jacob Johnson <johjacb@bethel.edu>
2018-04-06 10:23:37 -0500

    newt ready with most content

--- src/js/anime--custom.js
+++ src/js/anime--custom.js
@@ -1,27 +1,23 @@
-const hzBr = document.querySelectorAll(".horizontalBorder");
+const hzBr = document.querySelectorAll('#academics h1')
 
 if (hzBr.length > 0) {
-    window.anime = require('animejs')
+  window.anime = require('animejs')
 }
 
 const animateBorderTimeline = anime.timeline({
   delay: 0,
-  duration: 500,
-  easing: "easeOutCubic",
+  duration: 1000,
+  easing: 'easeOutCubic',
   loop: false
 })
 
-function aBF (hB, vB) {
-  animateBorderTimeline
-    .add({
-      targets: hB,
-      width: [0, "100%"]
-    })
-    .add({
-      offset: 50,
-      targets: vB,
-      height: [0, "100%"]
-    })
-}
-
-export { aBF }
+animateBorderTimeline
+  .add({
+    targets: hzBr,
+    opacity: [0, 1]
+  })
+  .add({
+    offset: 50,
+    targets: hzBr,
+    translateY: -50
+  })
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const path = require('path')
 module.exports = {
   entry: './src/js/main.js',
   output: {
-    filename: 'webpack.js'
+    filename: 'glaze.js'
   },
   module: {
     noParse: [/.\/src\/js\/vendor\/odometer.min.js/],
