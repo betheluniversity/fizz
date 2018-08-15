ff7151a67c444f26a2f8acb1c8b475907e7c967e
Jacob Johnson <johjacb@bethel.edu>
2018-08-15 10:34:05 -0500

    tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ import './accordion.js'
 import './bu_animate.js'
 import './action-nav.js'
 import './responsive-tables.js'
-import './lory--custom.js'
+// import './lory--custom.js'
 
 import svg4everybody from 'svg4everybody'
 svg4everybody()
